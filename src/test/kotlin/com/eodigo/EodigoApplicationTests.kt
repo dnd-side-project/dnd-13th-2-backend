030599b19b44aade6262be9a4fae6cf3286a71a9
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-05 00:08:32 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/kotlin/com/eodigo/EodigoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eodigo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EodigoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
