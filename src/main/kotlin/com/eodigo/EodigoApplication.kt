17f7ea9abd95283b27ab95643d6469247a643d26
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-06 12:09:42 +0900

    feat: 프로젝트 환경 설정 (#1)
    * feat: 프로젝트 초기 설정
    
    * docs: PR 템플릿 추가

---
+++ src/main/kotlin/com/eodigo/EodigoApplication.kt
@@ -0,0 +1,11 @@
+package com.eodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EodigoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EodigoApplication>(*args)
+}
