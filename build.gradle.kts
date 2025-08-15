ac16c8140cf1e5401eef3a5f6bdbbbc59030e361
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-16 04:10:45 +0900

    build: spring-boot-starter-web 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories { mavenCentral() }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
