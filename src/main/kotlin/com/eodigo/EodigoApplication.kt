728e07d829eb722fb6b2ff05f3b96c122381fc2a
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-15 18:51:37 +0900

    feat: 환경별 설정 관리 체계 구축 (#5)
    * build: dotenv-kotlin 의존성 추가
    
    * feat: 환경별 application.yml 설정 파일 추가
    
    * chore: .env.example 추가 및 .gitignore 설정
    
    * fix: 애플리케이션 시작 시 .env가 로드되도록 수정
    
    * chore: EOF 개행 추가

--- src/main/kotlin/com/eodigo/EodigoApplication.kt
+++ src/main/kotlin/com/eodigo/EodigoApplication.kt
@@ -1,10 +1,17 @@
 package com.eodigo
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication class EodigoApplication
 
 fun main(args: Array<String>) {
+    dotenv {
+        systemProperties = true
+        ignoreIfMissing = true
+        ignoreIfMalformed = true
+    }
+
     runApplication<EodigoApplication>(*args)
 }
