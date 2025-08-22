0d96e3011d1380c144a1eb7b012d270ee3d68b43
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-22 19:15:41 +0900

    chore: 자료형 변경 및 주석처리

--- src/test/kotlin/com/eodigo/batch/KamisBatchIntegrationTest.kt
+++ src/test/kotlin/com/eodigo/batch/KamisBatchIntegrationTest.kt
@@ -1,34 +1,34 @@
 package com.eodigo.batch
 
-//import com.eodigo.domain.product.entity.Product
-//import com.eodigo.domain.product.entity.Region
-//import com.eodigo.domain.product.enums.ProductSource
-//import com.eodigo.domain.product.repository.DailyRegionalPriceRepository
-//import com.eodigo.domain.product.repository.ProductRepository
-//import com.eodigo.domain.product.repository.RegionRepository
-//import com.eodigo.util.DotenvInitializer
-//import java.time.LocalDate
-//import org.assertj.core.api.Assertions.assertThat
-//import org.junit.jupiter.api.AfterEach
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.DisplayName
-//import org.junit.jupiter.api.Test
-//import org.springframework.batch.core.ExitStatus
-//import org.springframework.batch.core.JobParametersBuilder
-//import org.springframework.batch.test.JobLauncherTestUtils
-//import org.springframework.batch.test.context.SpringBatchTest
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.test.context.ActiveProfiles
-//import org.springframework.test.context.ContextConfiguration
-//import org.springframework.test.context.jdbc.Sql
+// import com.eodigo.domain.product.entity.Product
+// import com.eodigo.domain.product.entity.Region
+// import com.eodigo.domain.product.enums.ProductSource
+// import com.eodigo.domain.product.repository.DailyRegionalPriceRepository
+// import com.eodigo.domain.product.repository.ProductRepository
+// import com.eodigo.domain.product.repository.RegionRepository
+// import com.eodigo.util.DotenvInitializer
+// import java.time.LocalDate
+// import org.assertj.core.api.Assertions.assertThat
+// import org.junit.jupiter.api.AfterEach
+// import org.junit.jupiter.api.BeforeEach
+// import org.junit.jupiter.api.DisplayName
+// import org.junit.jupiter.api.Test
+// import org.springframework.batch.core.ExitStatus
+// import org.springframework.batch.core.JobParametersBuilder
+// import org.springframework.batch.test.JobLauncherTestUtils
+// import org.springframework.batch.test.context.SpringBatchTest
+// import org.springframework.beans.factory.annotation.Autowired
+// import org.springframework.boot.test.context.SpringBootTest
+// import org.springframework.test.context.ActiveProfiles
+// import org.springframework.test.context.ContextConfiguration
+// import org.springframework.test.context.jdbc.Sql
 //
-//@SpringBootTest
-//@SpringBatchTest
-//@Sql("/schema-h2.sql")
-//@ContextConfiguration(initializers = [DotenvInitializer::class])
-//@ActiveProfiles("test")
-//internal class KamisBatchIntegrationTest {
+// @SpringBootTest
+// @SpringBatchTest
+// @Sql("/schema-h2.sql")
+// @ContextConfiguration(initializers = [DotenvInitializer::class])
+// @ActiveProfiles("test")
+// internal class KamisBatchIntegrationTest {
 //
 //    @Autowired private lateinit var jobLauncherTestUtils: JobLauncherTestUtils
 //
@@ -40,8 +40,8 @@ package com.eodigo.batch
 //    fun setUp() {
 //        regionRepository.saveAll(
 //            listOf(
-//                Region(regionName = "서울", regionCode = 1101),
-//                Region(regionName = "부산", regionCode = 2100),
+//                Region(regionName = "서울", regionCode = "1101"),
+//                Region(regionName = "부산", regionCode = "2100"),
 //            )
 //        )
 //
@@ -49,21 +49,21 @@ package com.eodigo.batch
 //            listOf(
 //                Product(
 //                    name = "쌀 20kg",
-//                    categoryCode = 100,
+//                    categoryCode = "100",
 //                    categoryName = "식량작물",
-//                    itemCode = 111,
+//                    itemCode = "111",
 //                    itemName = "쌀",
-//                    kindCode = 1,
+//                    kindCode = "01",
 //                    kindName = "20kg",
 //                    source = ProductSource.KAMIS,
 //                ),
 //                Product(
 //                    name = "고랭지 배추",
-//                    categoryCode = 200,
+//                    categoryCode = "200",
 //                    categoryName = "채소류",
-//                    itemCode = 211,
+//                    itemCode = "211",
 //                    itemName = "배추",
-//                    kindCode = 2,
+//                    kindCode = "02",
 //                    kindName = "여름",
 //                    source = ProductSource.KAMIS,
 //                ),
@@ -96,4 +96,4 @@ package com.eodigo.batch
 //        assertThat(results[0].product).isNotNull
 //        assertThat(results[0].region).isNotNull
 //    }
-//}
+// }
