8406f2a66a549d9f0098dd1830f76a61ec064516
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-23 00:50:28 +0900

    build: Spring Batch 의존성 및 기본 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     // Spring Webflux
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/eodigo/EodigoApplication.kt
+++ src/main/kotlin/com/eodigo/EodigoApplication.kt
@@ -1,17 +1,12 @@
 package com.eodigo
 
 import io.github.cdimascio.dotenv.dotenv
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
-@EnableBatchProcessing
-@EnableJpaAuditing
-@SpringBootApplication
-class EodigoApplication
+@EnableScheduling @EnableJpaAuditing @SpringBootApplication class EodigoApplication
 
 fun main(args: Array<String>) {
     dotenv {
