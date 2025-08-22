010bcd6d8fcc504e0ead3798307057d9bde3e388
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-08-23 03:52:47 +0900

    test: 배치 관련 테스트 코드 삭제

--- src/test/kotlin/com/eodigo/EodigoApplicationTests.kt
+++ src/test/kotlin/com/eodigo/EodigoApplicationTests.kt
@@ -1,13 +1,10 @@
 package com.eodigo
 
-import com.eodigo.util.DotenvInitializer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
-@ContextConfiguration(initializers = [DotenvInitializer::class])
 @ActiveProfiles("test")
 class EodigoApplicationTests {
 
